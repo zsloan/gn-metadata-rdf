c7935255e2973fefa8e54a6f809bd77b2ae497eb
zsloan <zachary.a.sloan@gmail.com>
2020-11-19 15:39:04 -0600

    Add command line tool with datatype option using click + create database engine

--- gn_metadata_to_rdf/__main__.py
+++ gn_metadata_to_rdf/__main__.py
@@ -1,6 +1,20 @@
 
-import click
+import os
 
-@click.group()
-def cli():
-    pass
+import click
+from sqlalchemy import create_engine
+
+from datatypes import publications
+
+@click.command()
+@click.option('--datatype')
+def cli(datatype):
+    if datatype == "phenotypes":
+        db_engine = connect_db()
+        publications.convert(db_engine)
+
+
+def connect_db():
+    sql_uri = os.environ("SQL_URI")
+    engine = create_engine(sql_uri)
+    return engine
