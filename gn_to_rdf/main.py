50353a59edab67db542e9b41edee7fa60c43d66d
zsloan <zachary.a.sloan@gmail.com>
2021-03-05 20:28:08 +0000

    Added default output directory and option to include output directory as a parameter

--- gn_to_rdf/main.py
+++ gn_to_rdf/main.py
@@ -4,15 +4,19 @@ import sys
 import click
 from sqlalchemy import create_engine
 
-from datatypes import publications
+from datatypes import publications, phenotypes
+
+module_path = os.path.dirname(os.path.realpath(__file__))
 
 @click.command()
-@click.option('--datatype')
-def cli(datatype):
-    if datatype == "phenotypes":
-        db_engine = connect_db()
-        publications.convert(db_engine)
-
+@click.option('-d', '--datatype', required=True)
+@click.option('-o', '--out_file', default=os.path.join(module_path, "output/"))
+def cli(datatype, out_file):
+    db_engine = connect_db()
+    if datatype == "publications":
+        publications.convert(db_engine, out_file)
+    elif datatype == "phenotypes":
+        phenotypes.convert(db_engine, out_file)
 
 def connect_db():
     sql_uri = os.getenv("SQL_URI")
